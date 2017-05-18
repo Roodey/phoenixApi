94c6380e9e4bc5acf52173d4d87d308abe7e8266
Roodey <joey8williams@gmail.com>
2017-05-18 19:47:46 -0400

    Heroku Time

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenixApi, PhoenixApi.Endpoint,
   http: [port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "example.com", port: 80],
+  secret_key_base: System.get_evn("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production

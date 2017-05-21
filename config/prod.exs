b1b09df5ef3913b6d71f4d6bb241918afe35c09d
Roodey <joey8williams@gmail.com>
2017-05-21 10:08:28 -0400

    Heroku Timesss

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ config :phoenixApi, PhoenixApi.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "https://pacific-lake-83262.herokuapp.com/api/session", port: 80],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  # cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :phoenixApi, PhoenixApi.Repo,
   adapter: Ecto.Adapters.Postgres,
